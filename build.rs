1ea2f89540d28c85ce3d668302361d315f251b3a
Will Usher <willusher.life@gmail.com>
2016-04-14 14:17:15 -0600

    Always print the re-run text even if compilation fails, handle opt-level

--- build.rs
+++ build.rs
@@ -3,13 +3,26 @@ use std::process::Command;
 use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-changed=src/say_hello.ispc");
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let debug = env::var("DEBUG").unwrap();
+    let opt_level = env::var("OPT_LEVEL").unwrap();
 
     let mut ispc_args = vec!["src/say_hello.ispc", "--pic"];
     if debug == "true" {
         ispc_args.push("-g");
     }
+    if opt_level == "0" {
+        ispc_args.push("-O0");
+    } else if opt_level == "1" {
+        ispc_args.push("-O1");
+    } else if opt_level == "2" {
+        ispc_args.push("-O2");
+    } else if opt_level == "3" {
+        ispc_args.push("-O3");
+    }
+
     // Invoke ISPC to compile our code
     let ispc_status = Command::new("ispc").args(&ispc_args[..])
         .args(&["-o", &format!("{}/say_hello.o", out_dir)])
@@ -25,6 +38,5 @@ fn main() {
         panic!("Linking ISPC code into archive failed");
     }
     println!("cargo:rustc-flags=-L native={}", out_dir);
-    println!("cargo:rerun-if-changed=src/say_hello.ispc");
 }
 
