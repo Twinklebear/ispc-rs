c3df3a52ecd9bd8d95cbe10290246a164b325cea
Will Usher <willusher.life@gmail.com>
2016-04-16 16:03:54 -0600

    Remove trailing space

--- examples/simple/build.rs
+++ examples/simple/build.rs
@@ -1,7 +1,7 @@
 extern crate ispc;
 
 fn main() {
-    // Only re-run the build script if the ISPC files have been changed 
+    // Only re-run the build script if the ISPC files have been changed
     let ispc_files = vec!["src/simple.ispc"];
     for s in &ispc_files[..] {
         println!("cargo:rerun-if-changed={}", s);
