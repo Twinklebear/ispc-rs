84d385a8624eb090efe237c89d6cca9a297a9a4a
Will Usher <willusher.life@gmail.com>
2016-05-05 16:14:40 -0600

    Add scanline parallel mandelbrot

--- examples/mandelbrot/src/main.rs
+++ examples/mandelbrot/src/main.rs
@@ -12,8 +12,9 @@ fn main() {
     let max_iters = 255;
     let mut counts = vec![0; width * height];
     unsafe {
-        mandelbrot::mandelbrot(x.0, x.1, y.0, y.1, width as i32, height as i32,
-                               max_iters, counts.as_mut_ptr());
+        mandelbrot::mandelbrot(x.0, x.1, y.0, y.1, width as i32, height as i32, max_iters, counts.as_mut_ptr());
+        //mandelbrot::mandelbrot_tasks(x.0, x.1, y.0, y.1, width as i32, height as i32,
+        //                             max_iters, counts.as_mut_ptr());
     }
     // Convert the image to grey scale u8 format for saving
     let img = counts.iter().map(|x| 255 - *x as u8).collect::<Vec<u8>>();
