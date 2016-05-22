6474a0ed79962fc956787a87636f513643b0769b
Will Usher <willusher.life@gmail.com>
2016-05-22 11:35:59 -0500

    Add plane geometry and now intersect over a list of scene geometry

--- examples/rt/src/lib.rs
+++ examples/rt/src/lib.rs
@@ -13,6 +13,7 @@ ispc_module!(rt);
 pub use vec3f::Vec3f;
 pub use camera::Camera;
 pub use sphere::Sphere;
+pub use plane::Plane;
 pub use lights::PointLight;
 
 /// Type alias for the Geometry base struct in ISPC
@@ -21,6 +22,7 @@ pub type Geometry = ::rt::Struct_Geometry;
 pub mod vec3f;
 pub mod camera;
 pub mod sphere;
+pub mod plane;
 pub mod lights;
 
 pub fn render() {
@@ -29,11 +31,13 @@ pub fn render() {
     let camera = Camera::new(Vec3f::new(0.0, 0.0, -2.0), Vec3f::new(0.0, 0.0, 1.0),
                              Vec3f::new(0.0, 1.0, 0.0), 65.0, width, height);
     let sphere = Sphere::new(Vec3f::new(0.0, 0.0, 0.0), 0.5);
+    let plane = Plane::new(Vec3f::new(0.0, -0.5, 0.0), Vec3f::new(0.0, 1.0, 0.0));
     let light = PointLight::new(Vec3f::new(0.5, 0.5, -1.0), Vec3f::broadcast(1.0));
     let mut img_buf = vec![0.0; width * height * 3];
     let mut rng = rand::thread_rng();
     unsafe {
-        rt::render(&camera as *const Camera, sphere.ispc_equiv(), light.ispc_equiv(),
+        let geom = vec![sphere.ispc_equiv(), plane.ispc_equiv()];
+        rt::render(&camera as *const Camera, geom.as_ptr(), geom.len() as i32, light.ispc_equiv(),
                    rng.gen::<i32>(), width as i32, height as i32, img_buf.as_mut_ptr());
     }
     // Convert the image to RGB u8 to save
