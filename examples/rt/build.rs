67e3b14215dfd98501bd82c2c9982d83b3aecbf8
Will Usher <willusher.life@gmail.com>
2016-06-02 19:35:58 -0500

    Split various ispc headers into header + code files
    This shows a more interesting and complex multi-file example than the
    multi_file case (and it's easier to read)

--- examples/rt/build.rs
+++ examples/rt/build.rs
@@ -1,6 +1,18 @@
+
 extern crate ispc;
 
 fn main() {
-    ispc::compile_library("rt", &["src/rt.ispc"]);
+    // We need to use a custom config to explicitly not generate debug info
+    // for ISPC code on windows otherwise we get repeated symbol declarations
+    let mut cfg = ispc::Config::new();
+    if cfg!(windows) {
+        cfg.debug(false);
+    }
+    let ispc_files = vec!["src/rt.ispc", "src/geom.ispc",
+                          "src/material.ispc", "src/lights.ispc"];
+    for s in &ispc_files[..] {
+        cfg.file(*s);
+    }
+    cfg.compile("rt")
 }
 
