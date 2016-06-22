974c21a82a9596296ffc07aa2622e4e24f696aef
Will Usher <willusher.life@gmail.com>
2016-06-22 15:29:45 +0200

    Fix pathtracing and add back wall as well to contain light
    Also stop using float_to_srgb8 it seems to have very funny bugs???

--- examples/rt/src/lib.rs
+++ examples/rt/src/lib.rs
@@ -27,18 +27,22 @@ pub fn render() {
     let height = 512;
     let camera = Camera::new(Vec3f::new(0.0, 0.0, -3.0), Vec3f::new(0.0, 0.0, 0.0),
                              Vec3f::new(0.0, 1.0, 0.0), 60.0, width, height);
-    let white_mat = Lambertian::new(Vec3f::new(1.0, 1.0, 1.0));
-    let red_mat = Lambertian::new(Vec3f::new(1.0, 0.1, 0.1));
+    let white_mat = Lambertian::new(Vec3f::new(0.9, 0.9, 0.9));
+    let red_mat = Lambertian::new(Vec3f::new(0.8, 0.1, 0.1));
+    let blue_mat = Lambertian::new(Vec3f::new(0.1, 0.2, 0.7));
     let sphere = Sphere::new(Vec3f::new(0.0, 0.0, 0.0), 0.5, red_mat);
-    let plane = Plane::new(Vec3f::new(0.0, -0.5, 0.0), Vec3f::new(0.0, 1.0, 0.0), white_mat);
+    let floor = Plane::new(Vec3f::new(0.0, -0.5, 0.0), Vec3f::new(0.0, 1.0, 0.0), white_mat);
+    let back_wall = Plane::new(Vec3f::new(0.0, 0.0, 2.0), Vec3f::new(0.0, -0.6, -1.0), blue_mat);
     let light = PointLight::new(Vec3f::new(0.75, 0.75, -2.0), Vec3f::broadcast(10.0));
     let mut framebuffer = vec![0.0; width * height * 3];
     let mut srgb_img_buf = vec![0u8; width * height * 3];
     let mut rng = rand::thread_rng();
+    // We need a random seed for each scanline of the image
+    let scanline_seeds: Vec<_> = rng.gen_iter::<i32>().take(height).collect();
     unsafe {
-        let geom = vec![sphere.ispc_equiv(), plane.ispc_equiv()];
+        let geom = vec![sphere.ispc_equiv(), floor.ispc_equiv(), back_wall.ispc_equiv()];
         rt::render(&camera as *const Camera, geom.as_ptr(), geom.len() as i32, light.ispc_equiv(),
-                   rng.gen::<i32>(), width as i32, height as i32, framebuffer.as_mut_ptr());
+                   scanline_seeds.as_ptr(), width as i32, height as i32, framebuffer.as_mut_ptr());
         rt::framebuffer_to_srgb(framebuffer.as_ptr(), srgb_img_buf.as_mut_ptr(), width as i32, height as i32);
     }
     match image::save_buffer("rt.png", &srgb_img_buf[..], width as u32, height as u32, image::RGB(8)) {
