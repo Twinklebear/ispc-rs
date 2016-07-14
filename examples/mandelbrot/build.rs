44eb263ac2256b762cfb11601edf9791dfdaf59b
Will Usher <willusher.life@gmail.com>
2016-07-13 21:54:56 -0500

    Turn off instrumenting in mandelbrot for now

--- examples/mandelbrot/build.rs
+++ examples/mandelbrot/build.rs
@@ -1,10 +1,6 @@
 extern crate ispc;
 
 fn main() {
-    let mut cfg = ispc::Config::new();
-    cfg.file("src/mandelbrot.ispc");
-    cfg.instrument();
-    cfg.compile("mandelbrot");
-    //ispc::compile_library("mandelbrot", &["src/mandelbrot.ispc"]);
+    ispc::compile_library("mandelbrot", &["src/mandelbrot.ispc"]);
 }
 
