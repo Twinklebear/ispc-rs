dbb4f301e71bb1cc6bdf8cf2825c80f8488652ef
Will Usher <willusher.life@gmail.com>
2016-07-14 20:43:07 -0500

    Release 0.3.0

--- README.md
+++ README.md
@@ -24,10 +24,10 @@ or ISPC tasks.
 build = "build.rs"
 
 [dependencies]
-ispc = "0.2.1"
+ispc = "0.3.0"
 
 [build-dependencies]
-ispc = "0.2.1"
+ispc = "0.3.0"
 ```
 
 Now you can use `ispc` to compile your code into a static library:
--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,10 @@
 //! build = "build.rs"
 //!
 //! [dependencies]
-//! ispc = "0.2.1"
+//! ispc = "0.3.0"
 //!
 //! [build-dependencies]
-//! ispc = "0.2.1"
+//! ispc = "0.3.0"
 //! ```
 //!
 //! Now you can use `ispc` to compile your code into a static library:
