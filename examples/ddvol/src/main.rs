049e1604f969ccbc5507ff022d438ee4cbe8f1cc
Will Usher <willusher.life@gmail.com>
2016-07-17 11:22:54 -0500

    Fix bug with setting region not starting a 0,0,0

--- examples/ddvol/src/main.rs
+++ examples/ddvol/src/main.rs
@@ -4,6 +4,7 @@ extern crate image;
 extern crate rand;
 
 use std::ptr;
+use std::iter;
 use std::time::Instant;
 
 use rand::Rng;
@@ -30,9 +31,31 @@ fn main() {
     let height = 512;
     let camera = Camera::new(Vec3f::new(2.0, 0.5, 2.0), Vec3f::new(0.5, 0.5, 0.5),
                              Vec3f::new(0.0, 1.0, 0.0), 60.0, width as i32, height as i32);
-    let volume = Volume::new(Vec3i::new(2, 1, 1));
-    let volume_data = [0.0, 1.0];//, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
-    volume.set_region(&volume_data[..], Vec3i::broadcast(0), Vec3i::new(2, 1, 1));
+    let volume = Volume::new(Vec3i::broadcast(64));
+    let volume_data: Vec<_> = iter::repeat(0.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::broadcast(0), Vec3i::broadcast(32));
+
+    let volume_data: Vec<_> = iter::repeat(1.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::new(32, 0, 0), Vec3i::broadcast(32));
+
+    let volume_data: Vec<_> = iter::repeat(2.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::new(0, 32, 0), Vec3i::broadcast(32));
+
+    let volume_data: Vec<_> = iter::repeat(3.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::new(32, 32, 0), Vec3i::broadcast(32));
+
+    let volume_data: Vec<_> = iter::repeat(4.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::new(0, 0, 32), Vec3i::broadcast(32));
+
+    let volume_data: Vec<_> = iter::repeat(5.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::new(32, 0, 32), Vec3i::broadcast(32));
+
+    let volume_data: Vec<_> = iter::repeat(6.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::new(0, 32, 32), Vec3i::broadcast(32));
+
+    let volume_data: Vec<_> = iter::repeat(7.0).take(32 * 32 * 32).collect();
+    volume.set_region(&volume_data[..], Vec3i::new(32, 32, 32), Vec3i::broadcast(32));
+
     let mut framebuffer = vec![0.0; width * height * 3];
     let mut srgb_img_buf = vec![0u8; width * height * 3];
     let mut rng = rand::thread_rng();
@@ -40,6 +63,7 @@ fn main() {
     let scanline_seeds: Vec<_> = rng.gen_iter::<i32>().take(height).collect();
     unsafe {
         let start = Instant::now();
+        println!("Rendering");
         ddvol::render(camera.ispc_equiv(), volume.ispc_equiv(), scanline_seeds.as_ptr(),
                       width as i32, height as i32, framebuffer.as_mut_ptr());
         let elapsed = start.elapsed();
