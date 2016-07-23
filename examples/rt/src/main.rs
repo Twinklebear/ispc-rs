f6debf5bfe7cf715149c4c85f5dccea453e0536c
Will Usher <willusher.life@gmail.com>
2016-07-23 18:54:05 -0500

    Add command line args for scene & image filename

--- examples/rt/src/main.rs
+++ examples/rt/src/main.rs
@@ -6,10 +6,13 @@ extern crate ispc;
 extern crate image;
 extern crate rand;
 extern crate serde_json;
+extern crate docopt;
+extern crate rustc_serialize;
 
 use std::time::Instant;
 
 use rand::Rng;
+use docopt::Docopt;
 
 use camera::Camera;
 use scene::Scene;
@@ -23,8 +26,25 @@ mod scene;
 
 ispc_module!(rt);
 
+const USAGE: &'static str = "
+Usage:
+  rt <scene> [options]
+  rt (-h | --help)
+
+Options:
+  -o OUT        Specify a file to writing the render to, defaults to 'rt.png'.
+  -h, --help    Show this message.
+";
+
+#[derive(RustcDecodable)]
+pub struct Args {
+    arg_scene: String,
+    flag_o: Option<String>,
+}
+
 fn main() {
-    let scene = Scene::load("./scenes/sphere_on_plane.json");
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
+    let scene = Scene::load(&args.arg_scene[..]);
     let mut framebuffer = vec![0.0; scene.width * scene.height * 3];
     let mut srgb_img_buf = vec![0u8; scene.width * scene.height * 3];
     let mut rng = rand::thread_rng();
@@ -40,7 +60,12 @@ fn main() {
         rt::framebuffer_to_srgb(framebuffer.as_ptr(), srgb_img_buf.as_mut_ptr(),
                                 scene.width as i32, scene.height as i32);
     }
-    match image::save_buffer("rt.png", &srgb_img_buf[..], scene.width as u32, scene.height as u32, image::RGB(8)) {
+    let out_file = match args.flag_o {
+        Some(s) => s.clone(),
+        None => String::from("rt.png"),
+    };
+    match image::save_buffer(&out_file[..], &srgb_img_buf[..], scene.width as u32, scene.height as u32,
+                             image::RGB(8)) {
         Ok(_) => println!("Rendered image saved to rt.png"),
         Err(e) => panic!("Error saving image: {}", e),
     };
