16f73be75575bf2261a5700728af9712a01dda19
Will Usher <willusher.life@gmail.com>
2018-08-21 18:53:34 -0700

    use simple as an example for target isas

--- examples/simple/build.rs
+++ examples/simple/build.rs
@@ -12,9 +12,14 @@ fn main() {
     for s in &ispc_files[..] {
         cfg.file(*s);
     }
-    cfg.target_isas(vec![ispc::opt::TargetISA::SSE4i32x4,
+    // For a portable program we can explicitly compile for each target ISA
+    // we want. Then ISPC will pick the correct ISA at runtime to call
+    // for the target CPU.
+    cfg.target_isas(vec![
+                    ispc::opt::TargetISA::SSE2i32x4,
+                    ispc::opt::TargetISA::SSE4i32x4,
+                    ispc::opt::TargetISA::AVX1i32x8,
                     ispc::opt::TargetISA::AVX2i32x8,
-                    ispc::opt::TargetISA::AVX11i32x8,
                     ispc::opt::TargetISA::AVX512KNLi32x16]);
     cfg.compile("simple");
 }
