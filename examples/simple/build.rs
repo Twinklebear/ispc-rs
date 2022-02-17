07d0dac4ad89bf7c861ca2682383873dd264a462
Will Usher <will@willusher.io>
2022-02-16 21:09:03 -0800

    arm/neon support in simple example

--- examples/simple/build.rs
+++ examples/simple/build.rs
@@ -5,19 +5,26 @@ extern crate ispc_rt;
 #[cfg(feature = "ispc")]
 fn link_ispc() {
     use ispc_compile::TargetISA;
+
+    #[cfg(target_arch = "x86_64")]
+    let target_isas = vec![
+        TargetISA::SSE2i32x4,
+        TargetISA::SSE4i32x4,
+        TargetISA::AVX1i32x8,
+        TargetISA::AVX2i32x8,
+        TargetISA::AVX512KNLi32x16,
+        TargetISA::AVX512SKXi32x8,
+    ];
+
+    #[cfg(target_arch = "aarch64")]
+    let target_isas = vec![TargetISA::Neoni32x4];
+
     // For a portable program we can explicitly compile for each target ISA
     // we want. Then ISPC will pick the correct ISA at runtime to call
     // for the target CPU.
     ispc_compile::Config::new()
         .file("src/simple.ispc")
-        .target_isas(vec![
-            TargetISA::SSE2i32x4,
-            TargetISA::SSE4i32x4,
-            TargetISA::AVX1i32x8,
-            TargetISA::AVX2i32x8,
-            TargetISA::AVX512KNLi32x16,
-            TargetISA::AVX512SKXi32x8,
-        ])
+        .target_isas(target_isas)
         .out_dir("src/")
         .compile("simple");
 }
