007b06de9120a5485b44569d4dc40d1886b16eff
Will Usher <will@willusher.io>
2022-06-29 11:41:44 -0700

    demonstrate allowlist use in simple example

--- examples/simple/build.rs
+++ examples/simple/build.rs
@@ -19,12 +19,17 @@ fn link_ispc() {
     #[cfg(target_arch = "aarch64")]
     let target_isas = vec![TargetISA::Neoni32x4];
 
+    let bindgen_opts = ispc_compile::BindgenOptions {
+        allowlist_functions: vec![std::borrow::Cow::Borrowed(&"add_lists")],
+    };
+
     // For a portable program we can explicitly compile for each target ISA
     // we want. Then ISPC will pick the correct ISA at runtime to call
     // for the target CPU.
     ispc_compile::Config::new()
         .file("src/simple.ispc")
         .target_isas(target_isas)
+        .bindgen_options(bindgen_opts)
         .out_dir("src/")
         .compile("simple");
 }
